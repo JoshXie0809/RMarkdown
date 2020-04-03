6bfbf8708a4d9a27df56ab4c927ae50bf97d2668
JoshXie0809 <61863381+JoshXie0809@users.noreply.github.com>
2020-04-03 09:47:22 +0800

    no

--- lesson_R_7/func/percentage.R
+++ lesson_R_7/func/percentage.R
@@ -1,9 +1,8 @@
-#' Percentage
-#' `percetage()` change x to x1 % form, x1 = x * 100 , digits =2 
+#' percentage()
+#' change x to x1 % form, x1 = x * 100 , digits =2 
 #'
 percentage<-function(x, na.rm = T)
 {
-  
   x.percentage<-round(x*100,digits = 2)
-  paste(x.percentage,"%",collapse = ' ')
+  paste(x.percentage,"%",sep = '')
 }
